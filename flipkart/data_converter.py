1a8fd69959862730ab3124f3dff238673431b9ee
Ankush Singh <singhankush06@gmail.com>
2026-06-04 13:04:36 +0530

    Initial commit

---
+++ flipkart/data_converter.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+class DataConverter:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+    def convert(self):
+        df= pd.read_csv(self.data_path)[['product_title','review']]
+        docs=[
+            Document(page_content=row['review'], metadata={"product_title": row['product_title']})
+            for _, row in df.iterrows()
+        ]
+        return docs
---
+++ flipkart/data_ingestion.py
@@ -0,0 +1,26 @@
+from langchain_astradb import AstraDBVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+from flipkart.data_converter import DataConverter
+from flipkart.config import Config
+
+class DataIngestor:
+    def __init__(self):
+        self.embeddings = HuggingFaceEmbeddings(model=Config.EMBEDDING_MODEL)
+        self.vstore= AstraDBVectorStore(
+            api_endpoint=Config.ASTRA_DB_API_ENDPOINT,
+            token=Config.ASTRA_DB_APPLICATION_TOKEN,
+            namespace=Config.ASTRA_DB_KEYSPACE,
+            collection_name="flipkartdB",
+            embedding=self.embeddings
+        )
+    def ingest(self, load_existing):
+        if load_existing == True:
+            return self.vstore
+        docs=DataConverter("data\\flipkart_product_review.csv").convert()
+        self.vstore.add_documents(docs)
+        return self.vstore
+    
+
+if __name__ == "__main__":
+    ingestor = DataIngestor()
+    ingestor.ingest(load_existing=False)
